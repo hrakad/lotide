84d259afd42a43213f2cb9bce9337f6459c2f476
hrakad <your@email.com>
2021-07-24 08:57:13 +0000

    Implement head

---
+++ head.js
@@ -0,0 +1,9 @@
+const assertEqual = function(array) {
+  console.log(array[0]);
+};
+  // TEST CODE
+   
+assertEqual([5,6,7]);
+assertEqual(["Hello", "Lighthouse", "Labs"]);
+assertEqual([1]);
+assertEqual([]);
