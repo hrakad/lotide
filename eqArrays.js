a0c0351d256e63d37384cb6b4d4413d9029b5e84
hrakad <your@email.com>
2021-07-26 21:44:38 +0000

    factor

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,17 @@
 const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i] && arr1.every((value, index) => value === arr2[index])) {
-      console.log(`Assertion passed , ${arr1} === ${arr2}`);
-      break;
-    } else {
-      console.log(`Assertion failed , ${arr1} !== ${arr2}`);
-      break;
-    }
+    let result = '';
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i] && arr1.every((value, index) => value === arr2[index])) {
+          result = true;
+        } else {
+          result = false;
+        }
+      }
+    return result;
   }
-};
+console.assert(eqArrays([1, 2, 3], [1, 2, 3]));  
 eqArrays([1, 2, 3], [1, 2, 3]);
 eqArrays([1, 2, 3], [3, 2, 1]);
 eqArrays(["1", "2", "4"], ["1", "2", 4]);
-eqArrays(["1", "2", "4"], ["1", "2", "4"]);
-eqArrays(["a,b"],["a","b"]);
-
+eqArrays(["1", "2","4"], ["1","2", "4"]);
+eqArrays(["a,b"], ["a","b"]);
