560f1be184a510ae5387b36d295bf53e42aeeb0d
hrakad <your@email.com>
2021-07-27 07:23:42 +0000

    refactor

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(Arr) {
+    let flattenArray = [];
+    for (let i of Arr) {
+      if (Array.isArray(i)) {
+        flattenArray.push(...i);
+      } else {
+        flattenArray.push(i);
+      }
+    }
+    return flattenArray;
+  };
+  
+  console.log(flatten([1, 2, [3, 4], 5, [6]]));
