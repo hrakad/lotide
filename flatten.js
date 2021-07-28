3f3b88146902feb477ffad3a8e44eb517376769c
hrakad <your@email.com>
2021-07-28 21:33:44 +0000

    refactor

--- flatten.js
+++ flatten.js
@@ -1,3 +1,24 @@
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Array passed , ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Array failed , ${arr1} !== ${arr2}`);
+  }
+}; 
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
 const flatten = function(Arr) {
     let flattenArray = [];
     for (let i of Arr) {
