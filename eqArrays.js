eb7ed9c5e208e289980b5b2e95455c2c5ccceaf7
hrakad <your@email.com>
2021-07-29 06:04:06 +0000

    refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(` ✅✅✅ Assertion passed  , ${actual} === ${expected}`);
@@ -14,6 +5,15 @@ const assertEqual = function(actual, expected) {
     console.log(` 🛑🛑🛑 Assertion failed  , ${actual} != ${expected}`);
   }
 };
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
