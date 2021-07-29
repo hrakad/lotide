e6614cdbb327f10d36a2c089cfcd0dae5b8db9ca
hrakad <your@email.com>
2021-07-29 06:01:27 +0000

    refactor

--- tail.js
+++ tail.js
@@ -1,7 +1,7 @@
 const assertEqual = function(array) {
-   for (let i = 0; i < array.length ; i++) {
-    if (array.length <= 1 ) {
-        console.log(`🛑🛑🛑 Assertion failed`  , array[0]);
+  for (let i = 0; i < array.length; i++) {
+    if (array.length <= 1) {
+      console.log(`🛑🛑🛑 Assertion failed`  , array[0]);
     } else if (array.length >= 2) {
       console.log(`✅✅✅  assetion passed`, array.slice(1));
       break;
@@ -10,8 +10,8 @@ const assertEqual = function(array) {
 };
   // TEST CODE
   
-  assertEqual(["Hello", "Lighthouse Labs", "Bootcamp"]);
-  assertEqual([0, 1, 3, 4, 5]);
-  assertEqual(["Hello", "World"]);
-  assertEqual([3]);
-  assertEqual([1,2]);
+assertEqual(["Hello", "Lighthouse Labs", "Bootcamp"]);
+assertEqual([0, 1, 3, 4, 5]);
+assertEqual(["Hello", "World"]);
+assertEqual([3]);
+assertEqual([1,2]);
