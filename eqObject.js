59b85e721c10d458c174df43d0068340d17296f6
hrakad <your@email.com>
2021-08-10 02:53:42 +0000

    refactor

--- eqObject.js
+++ eqObject.js
@@ -7,29 +7,34 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  };
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
     return true;
-  };
+};
+
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-      return false;
-  } for (const key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-          if (!eqArrays(object1[key], object2[key])) {
-              return false;
-          } else if (object1[key] !== object2[key]) {
-              return false;
-          }
+    return false;
+  } else {
+    for (let key in object1) {
+      if (Array.isArray(object1[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+          return false;
       }
+    }
   }
   return true
-}; 
-
+};  
 const ab = { a : "1", b : "2"};
 const ba = { b : "2", a : "1"};
 const abc = { a: "1", b: "2", c: "3" };
@@ -40,4 +45,4 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), true);
 assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), true);
+assertEqual(eqObjects(cd, cd2), false);
