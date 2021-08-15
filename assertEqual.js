44669c5b669e2aab11bbe54c50009a9e8bccd2ab
hrakad <your@email.com>
2021-08-15 08:32:52 +0000

    refactor

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,16 @@
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(` ✅✅✅ Assertion passed  , ${actual} === ${expected}`);
   } else {
-    console.log(` 🛑🛑🛑 Assertion failed  , ${actual} != ${expected}`);
+    console.log(` 🛑🛑🛑 Assertion failed  , ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
- 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(3, 6);
 
+
+
+
+
+
+
+module.exports = assertEqual;
