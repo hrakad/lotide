07413733fc2940c1acb7ead60412376a311c79ff
Hatem Alahmad Alrakad <h.alrakad82@hotmail.com>
2021-08-14 23:40:09 -0600

    Update head.js

--- head.js
+++ head.js
@@ -1,9 +1,18 @@
-const assertEqual = function(array) {
-  console.log(array[0]);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion passed  , ${actual} === ${expected}`);
+  } else {
+    console.log(` 🛑🛑🛑 Assertion failed  , ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  let result = array[0];
+  return result;
 };
   // TEST CODE
    
-assertEqual([5,6,7]);
-assertEqual(["Hello", "Lighthouse", "Labs"]);
-assertEqual([1]);
-assertEqual([]);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), undefined);
+assertEqual(head([]), 0);
