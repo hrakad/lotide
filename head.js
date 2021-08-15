cacf6b9e3f6dfb65ed931f7bb08e97f45a13a16c
hrakad <your@email.com>
2021-08-15 22:49:55 +0000

    refactor

--- head.js
+++ head.js
@@ -5,9 +5,7 @@ const head = function(array) {
   let result = array[0];
   return result;
 };
-  // TEST CODE
-   
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), undefined);
-assertEqual(head([]), 0);
+
+
+
+module.exports = head;
