34c2160e2318f3da83289972d735c1287b2a34ca
hrakad <your@email.com>
2021-08-15 09:05:47 +0000

    refactor

--- map.js
+++ map.js
@@ -1,29 +1,30 @@
 const map = function(array, callback) {
-    const results = [];
-    for (let item of array) {
-       results.push(callback(item)); 
-    //   console.log('item BEFORE: ', item);
-    //   console.log('item AFTER: ', callback(item));
-    //   console.log('---');
-    }
-    return results;
+  const results = [];
+  for (let item of array) {
+   results.push(callback(item)); 
+  console.log('item BEFORE: ', item);
+  console.log('item AFTER: ', callback(item));
+  //console.log('---');
   }
+  return results;
+};
 
-  const eqArrays = function(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
-    return true;
-  };
+  }
+  return true;
+}; 
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Array passed , ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Array failed , ${arr1} !== ${arr2}`);
+  }
   
-  const assertArraysEqual = function(arr1, arr2) {
-    if (eqArrays(arr1, arr2)) {
-      console.log(`✅✅✅ Assertion Array passed , ${arr1} === ${arr2}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Array failed , ${arr1} !== ${arr2}`);
-    }
 };
   
 const words = ["ground", "control", "to", "major", "tom"];
@@ -37,4 +38,4 @@ assertArraysEqual(results2, ["o", "n", "o", "j", "m"]);
 
 const results3 = map(words, word => word.length);
 console.log(results3);
-assertArraysEqual(results3, [6, 7, 2, 5, 2]);
+assertArraysEqual(results3, [6, 7, 2, 5, 4]);
