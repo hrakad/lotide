ed6d230347a1d5a3ee839bdb2861cbad4c24299e
hrakad <your@email.com>
2021-08-16 00:57:28 +0000

    refactor

--- middle.js
+++ middle.js
@@ -1,36 +1,25 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
   
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Array passed , ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Array failed , ${arr1} !== ${arr2}`);
-  }
-};
 
 const middle = function(array) {
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-  
-  } if (array.length > 2 && array.length % 2 === 1) {
-    result.push(array[Math.floor(array.length / 2)]);
-  } else {
-    result.push(array[Math.floor(array.length / 2) - 1]);
+  if (array.length <= 2) {
+    return [];
   }
 
-  return result;
+  let middleIndex = Math.ceil((array.length) / 2 - 1); 
+  const middleValues = [];
+
+  if (array.length > 2 && array.length % 2 === 0) { 
+    middleValues.push(array[middleIndex], array[middleIndex + 1]); 
+  } else if (array.length > 2 && array.length % 2 !== 0) {
+    middleValues.push(array[middleIndex]);
+  } 
+  return middleValues;
 };
 
-console.log(middle[1, 2]);
-console.log(middle[1, 2, 3]);
-console.log(middle[1, 2, 3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5], 3), [3]);
-//assertArraysEqual(middle([1, 2, 3, 4, 5],3) [4]);
-// assertArraysEqual(middle([1, 2, 3, 4], [3]));
+
+
+
+
+module.exports = middle;
