3f02eaee4a7c4b7a827bcd19de031f27e144ce76
hrakad <your@email.com>
2021-08-20 01:37:19 +0000

    Add a separated test file

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,6 +1,7 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 4], [1, 2, 3]);
 assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
